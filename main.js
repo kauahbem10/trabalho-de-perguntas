c804383d0f827269fa9c591bc95a180aff5f8d68
AriellyMickozs <arielly.mickozs@escola.pr.gov.br>
2026-08-20 09:15:00 -0300

    rtyui

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-const caixaPrincipal = document.querySelector(.caixa-principal)
-const caixaPerguntas = document.querySelector(.caixa-perguntas)
-const caixaAlternativas = document.querySelector(.caixa-alternativas)
-const caixaresultado = document.querySelector(.caixa-resultado)
-const textoResultado = document.querySelector(.texto-resultado)
+const caixaPrincipal = document.querySelector(".caixa-principal")
+const caixaPerguntas = document.querySelector(".caixa-perguntas")
+const caixaAlternativas = document.querySelector(".caixa-alternativas")
+const caixaresultado = document.querySelector(".caixa-resultado")
+const textoResultado = document.querySelector(".texto-resultado")
 
 
 
