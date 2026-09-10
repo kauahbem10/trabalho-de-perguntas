78e585f1a091abda25f7d43becd9036077144421
kauahbem10 <k.bem@escola.pr.gov.br>
2026-09-10 09:29:43 -0300

    oi

--- main.js
+++ main.js
@@ -13,11 +13,17 @@ const perguntas = [
         alternativas: [
             {
                 texto: "Isso é assustador!",
-                afirmacao: "No início ficou com medo . "
+                afirmacao: [ 
+                    "No início ficou com medo , "
+                "NO inicio ficou alegre"
+                ]
             },
             {
                 texto: "Isso é maravilhoso!",
-                afirmacao: "você parau para comversar."
+                afirmacao: [
+                    "você parau para comversar,"
+                    "voce fugio"
+                ]
             }
         ]
     },
@@ -26,11 +32,17 @@ const perguntas = [
         alternativas: [
             {
                 texto: "você fica no veículo e espera ele ",
-                afirmacao: "Conseguiu fazer de forma certa."
-            },
+                afirmacao:[
+                    "Conseguiu fazer de forma certa,"
+                    "fez tudo errado "
+                ]
+            }
             {
                 texto: "você sai rápido do veículo, e o policial está suspeitando.",
-                afirmacao: "conseguiu fazer ele ter suspeita de você ."
+                afirmacao:[ 
+                    "conseguiu fazer ele ter suspeita de você ,"
+                    "fazer voce se matar "
+                ]
             }
         ]
     },
@@ -39,11 +51,17 @@ const perguntas = [
         alternativas: [
             {
                 texto: "deu tudo certo .",
-                afirmacao: "agora ele mandanda você ir para casa."
+                afirmacao: [ 
+                    "agora ele mandanda você ir para casa,"
+                    "so corre "
+                ]
             },
             {
                 texto: "deu errado e agora tem que deixar eles verificarem seu carro.",
-                afirmacao: "eles estão olhando muito pro seu carro."
+                afirmacao: [
+                "eles estão olhando muito pro seu carro,"
+                "eles estao de olho em voce"
+                ]
             }
         ]
     },
@@ -52,11 +70,17 @@ const perguntas = [
         alternativas: [
             {
                 texto: "ja está em casa, de boa.",
-                afirmacao: "vendo tv."
+                afirmacao: [ 
+                    "vendo tv,"
+                    "vendo o sol nacer 4"
+                ]
             },
             {
                 texto: "eles encontraram uma arma.",
-                afirmacao: "está cheio de sangue, e agora?"
+                afirmacao: [
+                    "está cheio de sangue, e agora?"
+                    "voce morreu "
+                ]
             }
         ]
     },
@@ -65,11 +89,17 @@ const perguntas = [
         alternativas: [
             {
                 texto: "você corre muito pois os policiais acharam a arma do crime",
-                afirmacao: "E você é pego ."
+                afirmacao: [ 
+                    "E você é pego ,"
+                    "voce ta morto e foi parar no inferno "
+                ]
             },
             {
                 texto: "você tenta matar os policiais.",
-                afirmacao: "e acaba sendo baleado e morto no local. "
+                afirmacao: [ 
+                    "e acaba sendo baleado e morto no local, "
+                    "tudo acaba em um lugar sem voce"
+                ]
             }
         ]
     },
